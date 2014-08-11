750ed4d5463fd20945b8ce648f7149cd5cd33991
YourSimile <yzl@abcdefg.local>
2014-08-11 10:30:14 +0800

    Initial Commit

---
+++ SLTTests/SLTTests.m
@@ -0,0 +1,34 @@
+//
+//  SLTTests.m
+//  SLTTests
+//
+//  Created by yzl on 14-8-11.
+//  Copyright (c) 2014年 com.ccwl. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SLTTests : XCTestCase
+
+@end
+
+@implementation SLTTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
